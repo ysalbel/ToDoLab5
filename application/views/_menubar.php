1ff12606645d1ab9731d195d5614861250214d55
ysalbel <ysabelliu276@gmail.com>
2018-03-01 13:27:02 +0800

    finished task 2

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -3,8 +3,15 @@
  * Menu navbar, just an unordered list
  */
 ?>
-<ul class="nav nav-pills">
+<ul class="nav">
     {menudata}
     <li><a href="{link}">{name}</a></li>
     {/menudata}
+    <li class="dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#">User Role<b class="caret"></b></a>
+      <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+                  <li><a href="/roles/actor/Guest">Guest</a></li>
+                  <li><a href="/roles/actor/Owner">Owner</a></li>
+      </ul>
+    </li>   
 </ul>
