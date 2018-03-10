1dbc41584518342f8041368e9a83c006ee92889d
Yen Bui <ybui0210@gmail.com>
2018-03-09 23:27:18 -0800

    completed step 3,4,5

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -12,12 +12,43 @@ class TaskTest extends TestCase
       // Load CI instance normally
       $this->CI = &get_instance();
       $this->CI->load->model('task_entity');
-      $this->Task = new Task_entity();
+      $this->Task = new Task();
     }
+    
     public function testAgeSetter()
     {
-                $expected = 27;
-                $this->Task->age = 27;
-                $this->assertEquals(27,$this->Task->age);
+                $expected = 20;
+                $this->Task->age = 20;
+                $this->assertEquals(20,$this->Task->age);
+    }
+    
+    public function testSetFlag ($value){
+        $expected = 1;
+        $this->Task->flag = 1;
+        $this->assertEquals(1,$this->Task->flag);
+    }
+    
+    public function testSetGroup($value){
+        $expected = 2;
+        $this->Task->group = 2;
+        $this->assertEquals(2,$this->Task->group);
+    }
+    
+    public function testSetPriority($value){
+        $expected = 3;
+        $this->Task->priority = 3;
+        $this->assertEquals(3,$this->Task->priority);
+    }
+    
+    public function testSetSize($value){
+        $expected = 1;
+        $this->Task->size = 1;
+        $this->assertEquals(1,$this->Task->size);
+    }
+    
+    public function testSetStatus($value){
+        $expected = 1;
+        $this->Task->status = 1;
+        $this->assertEquals(1,$this->Task->status);
     }
   }
